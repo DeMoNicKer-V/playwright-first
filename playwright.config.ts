92ab894f41bea8120509e2c83d85b594bd05f995
Victor Shakov <v.shakau@andersenlab.com>
2026-04-22 20:12:44 +0300

    Fixed env

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: process.env.BASE_URL,
+    baseURL: "https://practice.sdetunicorns.com",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-failure",
