0aa0022c774ea5f373a2b09c55df0e49f4978829
Victor Shakov <v.shakau@andersenlab.com>
2026-04-22 19:44:16 +0300

    Fixed locators

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -9,9 +9,11 @@ class HomePage {
   constructor(private page: Page) {
     this.headingText = page.locator("text=Think different. Make different");
     this.homeText = page.locator('#zak-primary-menu:has-text("Home")');
-    this.searchIcon = page.locator(
-      '//*[@class="zak-header-actions"]//*[@class="zak-icon zakra-icon--magnifying-glass"]',
-    );
+    this.searchIcon = page
+      .locator(
+        '//a[@class="zak-header-search__toggle"]//*[@class="zak-icon zakra-icon--magnifying-glass"]',
+      )
+      .first();
     this.navLinks = page.locator("#zak-primary-nav li[id*='menu-item']");
     this.getStartedBtn = page.locator("#get-started");
   }
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -13,30 +13,16 @@ test.describe("Home Page", () => {
     await expect(page).toHaveTitle("Practice E-Commerce Site – SDET Unicorns");
   });
 
-  test("Verify Heading Text is visible using Text Selector", async ({
-    page,
-  }) => {
-    const headingText = page.locator("text=Think different. Make different");
-    await expect(headingText).toBeVisible();
+  test("Verify Heading Text is visible using Text Selector", async () => {
+    await expect(homePage.headingText).toBeVisible();
   });
 
-  test("Verify Home Link is enabled using Text and CSS Selectors", async ({
-    page,
-  }) => {
-    // const homeText = page.locator("#zak-primary-menu >> text=Home");
-    const homeText = page.locator('#zak-primary-menu:has-text("Home")');
-
-    await expect(homeText).toBeEnabled();
+  test("Verify Home Link is enabled using Text and CSS Selectors", async () => {
+    await expect(homePage.homeText).toBeEnabled();
   });
 
-  test("Verify Search Icon is visible using XPath Selector", async ({
-    page,
-  }) => {
-    // const homeText = page.locator("#zak-primary-menu >> text=Home");
-    const searchIcon = page.locator(
-      '//*[@class="zak-header-actions"]//*[@class="zak-icon zakra-icon--magnifying-glass"]',
-    );
-    await expect(searchIcon).toBeVisible();
+  test("Verify Search Icon is visible using XPath Selector", async () => {
+    await expect(homePage.searchIcon).toBeVisible();
   });
 
   test("Verify the Text of all Nav Links", async () => {
